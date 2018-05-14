589226eeaf7b8ed8db62a5b2477bcfa1e8db6bd0
ewesmen <wesley.menezes@ericsson.com>
2018-05-14 20:00:49 -0300

    upload project

--- src/main/java/kt_rest/architecture/application/services/CustomerController.java
+++ src/main/java/kt_rest/architecture/application/services/CustomerController.java
@@ -1,24 +1,17 @@
 package kt_rest.architecture.application.services;
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
-import kt_rest.architecture.application.Application;
 
 
 @RestController
